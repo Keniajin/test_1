4b3858fec9ee8494311bf23c6d00114d9bbbc820
keniajin <keniajin@gmail.com>
2024-03-10 18:15:34 +0300

    added ebola analysis file

---
+++ scripts/ebola.R
@@ -0,0 +1,22 @@
+#############################################
+# Ebola case study
+# Script for loading the data
+# Your NAME here
+# Date
+#############################################
+# Load packages -----------------------------------------------------------
+pacman::p_load(
+  rio,          # for importing data
+  here,         # for relative file paths
+  skimr,        # for reviewing the data
+  janitor,      # for cleaning data
+  epikit,       # for creating age categories
+  tidyverse     # for data management and visualization
+)
+# Import data -------------------------------------------------------------
+surv_raw <- import(here("data", "raw", "surveillance_linelist_20141201.csv"))
+
+# Exploratory analysis ----------------------------------------------------
+# (contents may vary)
+# column names
+names(surv_raw)
